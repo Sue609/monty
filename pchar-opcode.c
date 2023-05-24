ba22a732ed932e7f75d8d0c677e0e344b8bb94d6
Sue609 <susankamau609@gmail.com>
2023-05-24 07:43:56 -0700

    The opcode pchar prints the char at the top of the stack, followed by a new line.

---
+++ pchar-opcode.c
@@ -0,0 +1,29 @@
+#include "monty.h"
+
+
+/**
+ * pchar_stack - function that is responsible for printing the character
+ * at the topof the stack as per ASII value.
+ * @stack: a double poinetr that shows the top of the stack.
+ * @line_number: The number of the line to be printed.
+ */
+
+void pchar_stack(stack_t **stack, unsigned int line_number)
+{
+	int value;
+
+	if (*stack == NULL)
+	{
+		fprintf(stderr, "L%u: can't pchar, empty stack\n", line_number);
+		exit(EXIT_FAILURE);
+	}
+
+	value = (*stack)->n;
+
+	if (value < 0 || value > 127)
+	{
+		fprintf(stderr, "L%u: can't pchar, value out of range\n", line_number);
+		exit(EXIT_FAILURE);
+	}
+	printf("%c\n", value);
+}
