8001ccc9df8133ea30377e7acff12c0ab010487b
Sue609 <susankamau609@gmail.com>
2023-05-25 13:55:00 -0700

    The opcode pchar prints the char at the top of the stack, followed by a new line.

--- pchar-opcode.c
+++ pchar-opcode.c
@@ -12,17 +12,17 @@ void pchar_stack(stack_t **stack, unsigned int line_number)
 {
 	int value;
 
-	if (*stack == NULL)
-	{
-		fprintf(stderr, "L%u: can't pchar, empty stack\n", line_number);
-		exit(EXIT_FAILURE);
-	}
-
 	value = (*stack)->n;
 
 	if (value < 0 || value > 127)
 	{
 		fprintf(stderr, "L%u: can't pchar, value out of range\n", line_number);
+		exit(EXIT_FAILURE);
+	}
+
+	if (*stack == NULL)
+	{
+		fprintf(stderr, "L%u: can't pchar, empty stack\n", line_number);
 		exit(EXIT_FAILURE);
 	}
 	printf("%c\n", value);
